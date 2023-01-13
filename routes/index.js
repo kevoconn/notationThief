f3e74a28c8f15cf006fbbd95392fd7bb295d510f
github_kevoconn <kcoconnor84@gmail.com>
2023-01-13 14:01:49 -0600

    added routes folder,index,notes and server.js files

---
+++ routes/index.js
@@ -0,0 +1,10 @@
+const express = require("express");
+
+// Import our modular routers for /notes and /feedback
+const notesRouter = require("./notes");
+
+const app = express();
+
+app.use("/notes", notesRouter);
+
+module.exports = app;
---
+++ routes/notes.js
@@ -0,0 +1,31 @@
+const notes = require('express').Router();
+const { readFromFile, readAndAppend } = require('../helpers/fsUtils');
+const { v4: uuidv4 } = require('uuid');
+
+// GET Route for retrieving all the notes
+notes.get('/', (req, res) => {
+  readFromFile('./db/db.json').then((data) => res.json(JSON.parse(data)));
+});
+
+// // POST Route for a new UX/UI tip
+// notes.post('/', (req, res) => {
+//   console.log(req.body);
+
+//   const { username, topic, tip } = req.body;
+
+//   if (req.body) {
+//     const newTip = {
+//       username,
+//       tip,
+//       topic,
+//       tip_id: uuidv4(),
+//     };
+
+//     readAndAppend(newTip, './db/notes.json');
+//     res.json(`Tip added successfully 🚀`);
+//   } else {
+//     res.error('Error in adding tip');
+//   }
+// });
+
+module.exports = notes;
