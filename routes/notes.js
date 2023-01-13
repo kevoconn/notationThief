2e1ca3968cdc59ffc89f6a69fdec53d08933c3eb
github_kevoconn <kcoconnor84@gmail.com>
2023-01-13 14:19:52 -0600

    added post route

--- routes/notes.js
+++ routes/notes.js
@@ -7,25 +7,24 @@ notes.get('/', (req, res) => {
   readFromFile('./db/db.json').then((data) => res.json(JSON.parse(data)));
 });
 
-// // POST Route for a new UX/UI tip
-// notes.post('/', (req, res) => {
-//   console.log(req.body);
+// POST Route for a new Notes
+notes.post('/', (req, res) => {
+  console.log(req.body);
 
-//   const { username, topic, tip } = req.body;
+  const { title, text } = req.body;
 
-//   if (req.body) {
-//     const newTip = {
-//       username,
-//       tip,
-//       topic,
-//       tip_id: uuidv4(),
-//     };
+  if (req.body) {
+    const newNotes = {
+      title,
+      text,
+      note_id: uuidv4(),
+    };
 
-//     readAndAppend(newTip, './db/notes.json');
-//     res.json(`Tip added successfully 🚀`);
-//   } else {
-//     res.error('Error in adding tip');
-//   }
-// });
+    readAndAppend(newNotes, './db/db.json');
+    res.json(`Notes added successfully 🚀`);
+  } else {
+    res.error('Error in adding Notes');
+  }
+});
 
 module.exports = notes;
