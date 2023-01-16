3f623a0f1613dd3736d6266a2688f7616c022398
github_kevoconn <kcoconnor84@gmail.com>
2023-01-16 16:06:42 -0600

    fixed css issue

--- server.js
+++ server.js
@@ -10,6 +10,7 @@ const app = express();
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use("/api", api);
+app.use(express.static("public"));
 
 // GET Route for homepage
 app.get("/", (req, res) => res.sendFile(path.join(__dirname, "/public/index.html")));
