95fc0905e1ce58b4a53c3c464650d421ac554420
github_kevoconn <kcoconnor84@gmail.com>
2023-01-16 14:40:41 -0600

    added delete function

--- routes/notes.js
+++ routes/notes.js
@@ -1,14 +1,14 @@
-const notes = require('express').Router();
-const { readFromFile, readAndAppend } = require('../helpers/fsUtils');
-const { v4: uuidv4 } = require('uuid');
+const notes = require("express").Router();
+const { readFromFile, readAndAppend, writeToFile } = require("../helpers/fsUtils");
+const { v4: uuidv4 } = require("uuid");
 
 // GET Route for retrieving all the notes
-notes.get('/', (req, res) => {
-  readFromFile('./db/db.json').then((data) => res.json(JSON.parse(data)));
+notes.get("/", (req, res) => {
+  readFromFile("./db/db.json").then((data) => res.json(JSON.parse(data)));
 });
 
 // POST Route for a new Notes
-notes.post('/', (req, res) => {
+notes.post("/", (req, res) => {
   console.log(req.body);
 
   const { title, text } = req.body;
@@ -20,11 +20,23 @@ notes.post('/', (req, res) => {
       note_id: uuidv4(),
     };
 
-    readAndAppend(newNotes, './db/db.json');
+    readAndAppend(newNotes, "./db/db.json");
     res.json(`Notes added successfully 🚀`);
   } else {
-    res.error('Error in adding Notes');
+    res.error("Error in adding Notes");
   }
+});
+
+notes.delete("/:id", (req, res) => {
+  const noteId = req.params.id;
+  readFromFile("./db/db.json")
+    .then((data) => JSON.parse(data))
+    .then((notes) => {
+      console.log(notes);
+      let filterNotes = notes.filter((note) => note.id !== noteId);
+      writeToFile("./db/db.json", filterNotes);
+      res.json(`Notes deleted successfully`);
+    });
 });
 
 module.exports = notes;
